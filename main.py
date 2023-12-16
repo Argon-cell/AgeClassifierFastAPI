0c95c81d1a35e03403266ee865ed5699e2fa24f9
Argon-cell <n4fikov.ilgam@gmail.com>
2023-12-16 17:58:26 +0300

    refactor: some lint fixes

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import re
 import validators
 from io import BytesIO
 import torch
-#%matplotlib inline
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from transformers import ViTFeatureExtractor, ViTForImageClassification
@@ -12,20 +11,18 @@ from transformers import ViTFeatureExtractor, ViTForImageClassification
 import uvicorn
 from fastapi import FastAPI
 
-
-
 app = FastAPI()
 
 @app.get("/get-age-by-photo")
 async def get_image(url):
     if validators.url(url) is True:
-
         im = Image.open(requests.get(url, stream=True).raw)
         print(im)
 
         # Init model, transforms
         model = ViTForImageClassification.from_pretrained('nateraw/vit-age-classifier')
         transforms = ViTFeatureExtractor.from_pretrained('nateraw/vit-age-classifier')
+
         # Transform our image and pass it through the model
         inputs = transforms(im, return_tensors='pt')
         output = model(**inputs)
@@ -37,10 +34,10 @@ async def get_image(url):
         preds = proba.argmax(1)
         imgplot = plt.imshow(im)
         plt.show()
-        # print(output.logits)
         a = {'0': "0-2", '1': "3-9", '2':  "10-19", '3': "20-29", '4': "30-39", '5': "40-49", '6': "50-59", '7': "60-69", '8': "more than 70"}
         number = re.findall(r'\d+', str(preds[0]))
         result = a[number[0]]
+        
         return {"Age": result}
     else:
         return {"Age": "This is not url"}
